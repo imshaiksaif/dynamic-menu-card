41bb47367de93ad705f330bbe92d97cb580b9486
imshaiksaif <imshaiksaif@gmail.com>
2020-05-13 14:29:25 +0530

    modified homepage

---
+++ build/precache-manifest.aee52cefd0663062aec829cc2fb6decb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8d8c6060c5764da2a3834844464034",
+    "url": "/dynamic-menu-card/index.html"
+  },
+  {
+    "revision": "27e9a074d538177d0d91",
+    "url": "/dynamic-menu-card/static/css/main.e16bb295.chunk.css"
+  },
+  {
+    "revision": "2752e766e15bee71229c",
+    "url": "/dynamic-menu-card/static/js/2.2f2f71bd.chunk.js"
+  },
+  {
+    "revision": "9b50f5741be789e4838182ccc7c0c6a5",
+    "url": "/dynamic-menu-card/static/js/2.2f2f71bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e9a074d538177d0d91",
+    "url": "/dynamic-menu-card/static/js/main.9c6022bc.chunk.js"
+  },
+  {
+    "revision": "224607b285aced0f7b39",
+    "url": "/dynamic-menu-card/static/js/runtime-main.5e43bfd0.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/dynamic-menu-card/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/dynamic-menu-card/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/dynamic-menu-card/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/dynamic-menu-card/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/dynamic-menu-card/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/dynamic-menu-card/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/dynamic-menu-card/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/dynamic-menu-card/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/dynamic-menu-card/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
